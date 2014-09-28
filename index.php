7eb8c65f76872853757640a04f911457a45bfa91
Asalehh <ahmed@nilehoster.com>
2014-09-28 12:31:04 +0200

    small html/css update in index.php

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ $password = generatePassword($wordsCount,$wordsShape,$separator,$insertNumber,$i
                 	Know more about <a href="http://xkcd.com/936/">xkcd password strength</a><br>
                 
                 	<a href="http://xkcd.com/936/">
-                		<img style="width:600px; height:450px;" src="http://imgs.xkcd.com/comics/password_strength.png" alt="xkcd style passwords">
+                		<img class="xkcdimage" src="http://imgs.xkcd.com/comics/password_strength.png" alt="xkcd style passwords">
                 	</a>
                 
             </div>
